da56838865511a53a179f94cf196a56f3a082a47
vinod.cm46@gmail.com <vinod.cm46@gmail.com@ffc9cd14-7569-7919-a689-d50ac92714e5>
2014-12-18 17:18:07 +0000

    Commited by ........Vinnu

--- trunk/Shop Inventory/src/org/shop/web/controllers/MyController.java
+++ trunk/Shop Inventory/src/org/shop/web/controllers/MyController.java
@@ -5,9 +5,16 @@
 package org.shop.web.controllers;
 
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.shop.domain.Address;
+import org.shop.domain.Customer;
+import org.shop.utils.HibernateUtils;
+import org.shop.utils.InstanceUtils;
 import org.shop.web.utils.RootController;
 
 /**
@@ -24,5 +31,17 @@ public class MyController extends RootController {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println(username + password);
+         InstanceUtils utils = new InstanceUtils();
+        Address address = utils.getAddress();
+        Customer customer = utils.getCustomer();
+        address.setDistrictName("aaa");
+        address.setPinCode(Long.MIN_VALUE);
+        customer.setAddress(address);
+        customer.setCustomer_Join_Date(new Date());
+        customer.setCustomer_Name("bbb");
+        Session session = HibernateUtils.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(customer);
+        HibernateUtils.closeSession(session, transaction);
     }
 }
