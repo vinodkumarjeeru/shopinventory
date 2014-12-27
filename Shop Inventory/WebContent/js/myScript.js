ec9b416fbdc458ee23a45e446296f727394182cf
vinod.cm46@gmail.com <vinod.cm46@gmail.com@ffc9cd14-7569-7919-a689-d50ac92714e5>
2014-12-27 16:42:25 +0000

    Updated document added by .....Vinnu

--- Shop Inventory/WebContent/js/myScript.js
+++ Shop Inventory/WebContent/js/myScript.js
@@ -2,19 +2,29 @@ function showRegForm() {
     $('#customerRegForm').slideDown(300);
     $('#checkCustomerBalance').slideUp(300);
     $('#storeItemsList').hide(1000);
+    $('#updateAddress').hide("slide", {direction: "down"}, 1000);
 }
 function checkBalanceForm() {
     $('#checkCustomerBalance').slideDown(300);
     $('#customerRegForm').slideUp(300);
     $('#storeItemsList').hide(1000);
+    $('#updateAddress').hide("slide", {direction: "down"}, 1000);
 }
 function getStoreItemList() {
     $('#customerRegForm').slideUp(300);
     $('#checkCustomerBalance').slideUp(300);
     $('#storeItemsList').show(1000);
+    $('#updateAddress').hide("slide", {direction: "down"}, 1000);
 }
 function viewCustomerDetails() {
     $('#customerRegForm').slideUp(300);
     $('#checkCustomerBalance').slideUp(300);
     $('#storeItemsList').hide(1000);
+    $('#updateAddress').hide("slide", {direction: "down"}, 1000);
+}
+function updateAddress() {
+    $('#customerRegForm').hide("slide", {direction: 'right'}, 300);
+    $('#checkCustomerBalance').hide("slide", {direction: 'right'}, 300);
+    $('#storeItemsList').hide("slide", {direction: 'right'}, 300);
+    $('#updateAddress').toggle("slide", {direction: "left"}, 500);
 }
