409b04241956861a9ba5dea38774cf37663b26b9
vinod.cm46@gmail.com <vinod.cm46@gmail.com@ffc9cd14-7569-7919-a689-d50ac92714e5>
2015-01-07 12:54:31 +0000

    Customer creation controller added by ... Vinnu

---
+++ trunk/Shop Inventory/src/org/shop/web/controllers/CreateCustomerController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.shop.web.controllers;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.shop.domain.Address;
+import org.shop.domain.Customer;
+import org.shop.service.InventoryService;
+import org.shop.service.impl.InventoryServiceImpl;
+import org.shop.utils.InstanceUtils;
+import org.shop.web.utils.RootController;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CreateCustomerController extends RootController {
+
+    @Override
+    protected void doWork(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String name = firstname + lastname;
+        Long mobile = new Long(request.getParameter("mobile"));
+        String door = request.getParameter("door");
+        String village = request.getParameter("village");
+        String district = request.getParameter("district");
+        String state = request.getParameter("state");
+        Long pincode = new Long(request.getParameter("pincode"));
+
+        InstanceUtils utils = new InstanceUtils();
+        Address address = utils.getAddress();
+        Customer customer = utils.getCustomer();
+        address.setDoorNo(door);
+        address.setStateName(state);
+        address.setDistrictName(district);
+        address.setPinCode(pincode);
+        address.setVilleageName(village);
+        customer.setAddress(address);
+        customer.setCustomer_Join_Date(new Date());
+        customer.setCustomer_Name(name);
+        customer.setCustomer_Phone(mobile);
+
+        InventoryService service = InventoryServiceImpl.getService();
+        service.createCustomer(customer);
+        request.setAttribute("status", "New Customer Successfully Created");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/createCustomer.jsp");
+        dispatcher.forward(request, response);
+    }
+}
--- trunk/Shop Inventory/src/org/shop/web/controllers/MyController.java
+++ trunk/Shop Inventory/src/org/shop/web/controllers/MyController.java
@@ -5,16 +5,11 @@
 package org.shop.web.controllers;
 
 import java.io.IOException;
-import java.util.Date;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.shop.domain.Address;
-import org.shop.domain.Customer;
-import org.shop.utils.HibernateUtils;
-import org.shop.utils.InstanceUtils;
+import javax.servlet.http.HttpSession;
 import org.shop.web.utils.RootController;
 
 /**
@@ -22,26 +17,40 @@ import org.shop.web.utils.RootController;
  * @author 3500512
  */
 public class MyController extends RootController {
-    
-    
-    
+
     @Override
     protected void doWork(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        System.out.println(username + password);
+        /*String username = request.getParameter("username");
+         String password = request.getParameter("password");
+         System.out.println(username + password);
          InstanceUtils utils = new InstanceUtils();
-        Address address = utils.getAddress();
-        Customer customer = utils.getCustomer();
-        address.setDistrictName("aaa");
-        address.setPinCode(Long.MIN_VALUE);
-        customer.setAddress(address);
-        customer.setCustomer_Join_Date(new Date());
-        customer.setCustomer_Name("bbb");
-        Session session = HibernateUtils.getSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(customer);
-        HibernateUtils.closeSession(session, transaction);
+         Address address = utils.getAddress();
+         Customer customer = utils.getCustomer();
+         address.setDistrictName("aaa");
+         address.setPinCode(Long.MIN_VALUE);
+         customer.setAddress(address);
+         customer.setCustomer_Join_Date(new Date());
+         customer.setCustomer_Name("bbb");
+         Session session = HibernateUtils.getSession();
+         Transaction transaction = session.beginTransaction();
+         session.save(customer);
+         HibernateUtils.closeSession(session, transaction);
+        
+         */
+        PrintWriter out = response.getWriter();
+
+        String rq = request.getParameter("login");
+
+        if (rq.equalsIgnoreCase("Login")) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            System.out.println(username + password);
+            HttpSession session = request.getSession();
+            session.setAttribute("User", username);
+            response.sendRedirect("index.jsp");
+
+        }
+
     }
 }
