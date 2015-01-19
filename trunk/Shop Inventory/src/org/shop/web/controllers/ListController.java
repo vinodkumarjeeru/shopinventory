c83492c4bcf4c2d9286e4e323a69d4f67e1542e8
vinod.cm46@gmail.com <vinod.cm46@gmail.com@ffc9cd14-7569-7919-a689-d50ac92714e5>
2015-01-19 17:10:56 +0000

    Added Customer list controller

---
+++ trunk/Shop Inventory/src/org/shop/web/controllers/ListController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.shop.web.controllers;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.shop.domain.Customer;
+import org.shop.service.InventoryService;
+import org.shop.utils.InstanceUtils;
+import org.shop.web.utils.RootController;
+
+/**
+ *
+ * @author vinod
+ */
+public class ListController extends RootController {
+
+    @Override
+    protected void doWork(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (request.getParameter("list").equalsIgnoreCase("customersList")) {
+            if (request.getSession().getAttribute("Admin") == null) {
+                response.sendRedirect("home.jsp");
+            } else {
+                InstanceUtils utils = new InstanceUtils();
+                InventoryService service = utils.getService();
+                List<Customer> list = service.getAllCustomers();
+                request.setAttribute("studentList", list);
+                request.getRequestDispatcher("/viewCustomers.jsp").forward(request, response);
+            }
+        }
+
+    }
+}
